3b6bceaba4d2c05d040a60d64117ace48e15a4a7
Daniel Kelly <me@danielkelly.io>
2022-07-13 14:25:39 -0500

    exercise 6

--- src/components/BoardDragAndDrop.vue
+++ src/components/BoardDragAndDrop.vue
@@ -1,70 +1,99 @@
-<script setup>
-import { watch, reactive, toRaw } from "vue";
+<script setup lang="ts">
 import { cloneDeep } from "lodash-es";
-import draggable from "vuedraggable";
+import { reactive, watch, toRaw } from "vue";
+import type { Board, Column, Task } from "@/types";
 import { v4 as uuidv4 } from "uuid";
+import draggable from "vuedraggable";
+import TaskCard from "./TaskCard.vue";
+import { useAlerts } from "@/stores/alerts";
+const alerts = useAlerts();
+// props
+const props = defineProps<{
+  board: Board;
+  tasks: Task[];
+  addTask(task: Partial<Task>): Task;
+}>();
 
-const props = defineProps({
-  board: Object,
-  tasks: Array,
-});
-
-// emits
-const emit = defineEmits(["update"]);
+// events
+const emit = defineEmits<{
+  (e: "update", payload: Partial<Board>): void;
+}>();
 
 // local data
 const tasks = reactive(cloneDeep(props.tasks));
 const board = reactive(cloneDeep(props.board));
-const columns = reactive(JSON.parse(board.order));
+const columns = reactive<Column[]>(JSON.parse(board.order as string));
 
-const addColumn = () =>
-  columns.push({ id: uuidv4(), title: "New column", taskIds: [] });
+// methods
+function addColumn() {
+  columns.push({
+    id: uuidv4(),
+    title: "New Column",
+    taskIds: [],
+  });
+}
 
-watch(columns, () =>
+watch(columns, () => {
   emit(
     "update",
-    cloneDeep({
-      ...props.board,
-      order: JSON.stringify(toRaw(columns)),
-    })
-  )
-);
+    cloneDeep({ ...board, order: JSON.stringify(toRaw(columns)) })
+  );
+});
+
+async function addTask({ column, title }: { column: Column; title: string }) {
+  const newTask = { title };
+  try {
+    const savedTask = await props.addTask(newTask);
+    tasks.push({ ...savedTask });
+    column.taskIds.push(savedTask.id);
+  } catch (error) {
+    alerts.error("Error creating task!");
+  }
+}
 </script>
 
 <template>
-  <button class="text-gray-500" @click="addColumn">New Column +</button>
-  <div class="flex items-start py-12">
-    <!-- //! outer block -->
+  <div class="flex py-12 items-start">
     <draggable
       :list="columns"
       group="columns"
       item-key="id"
-      class="flex flex-grow-0 flex-shrink-0 overflow-x-scroll"
+      class="flex overflow-x-scroll flex-grow-0 flex-shrink-0"
     >
       <template #item="{ element: column }">
         <div
           class="column bg-gray-100 flex flex-col justify-between rounded-lg px-3 py-3 rounded mr-4 w-[300px]"
         >
-          <h2>{{ column.title }}</h2>
-          <!-- //! inner block -->
-          <draggable
-            :list="column.taskIds"
-            group="tasks"
-            item-key="uid"
-            :animation="200"
-            ghost-class="ghost-card"
-            class="min-h-[400px]"
-          >
-            <template #item="{ element: taskId }">
-              <task-card
-                v-if="tasks.find((t) => t.id === taskId)"
-                :task="tasks.find((t) => t.id === taskId)"
-                class="mt-3 cursor-move"
-              />
-            </template>
-          </draggable>
+          <div>
+            <h3>{{ column.title }}</h3>
+            <draggable
+              :list="column.taskIds"
+              group="tasks"
+              item-key="uid"
+              :animation="200"
+              ghost-class="ghost-card"
+              class="min-h-[400px]"
+            >
+              <template #item="{ element: taskId }">
+                <TaskCard
+                  v-if="tasks.find((t: Task) => t.id === taskId)"
+                  :task="(tasks.find((t: Task) => t.id === taskId) as Task)"
+                  class="mt-3 cursor-move"
+                />
+              </template>
+            </draggable>
+            <TaskCreator
+              @create="
+                addTask({
+                  column,
+                  title: $event,
+                })
+              "
+            />
+          </div>
         </div>
       </template>
     </draggable>
+    <button class="text-gray-500" @click="addColumn">New Column +</button>
   </div>
 </template>
--- src/pages/boards/[id].vue
+++ src/pages/boards/[id].vue
@@ -1,9 +1,11 @@
-<script setup>
+<script setup lang="ts">
 import { ref, toRefs } from "vue";
 // import AppPageHeading from "../../components/AppPageHeading.vue";
 import BoardDragAndDrop from "../../components/BoardDragAndDrop.vue";
 // import { useAlerts } from "@/stores/Alerts";
 // const alerts = useAlerts();
+import type { Task } from "@/types";
+import { v4 as uuidv4 } from "uuid";
 
 const props = defineProps({
   id: String,
@@ -12,37 +14,52 @@ const props = defineProps({
 const { id: boardId } = toRefs(props);
 
 const board = ref({
-  id: boardId.value,
+  id: boardId?.value || "1",
   title: "Let's have an amazing time at Vue.js forge!! 🍍",
   order: JSON.stringify([
     { id: "1", title: "backlog 🌴", taskIds: ["1", "2"] },
   ]),
 });
 
-const tasks = ref([
+const tasks = ref<Partial<Task>[]>([
   { id: "1", title: "Code like mad people!" },
   { id: "2", title: "Push clean code" },
 ]);
+
+async function addTask(task: Task) {
+  return new Promise((resolve, reject) => {
+    const taskWithTheId = {
+      ...task,
+      id: uuidv4(),
+    };
+    tasks.value.push(taskWithTheId);
+    resolve(taskWithTheId);
+  });
+}
 
 const updateBoard = (b) => {
   board.value = b;
   // alerts.success("Board updated!");
 };
+
+const deleteBoardIfConfirmed = () => {
+  console.log("delete board");
+};
 </script>
 
 <template>
   <div>
-    <!-- <app-page-heading>
+    <AppPageHeading>
       {{ board.title }}
-    </app-page-heading> -->
+    </AppPageHeading>
+    <BoardMenu :board="board" @deleteBoard="deleteBoardIfConfirmed" />
 
-    <board-drag-and-drop :tasks="tasks" :board="board" @update="updateBoard" />
-
-    <details>
-      <pre>
-        {{ board }}
-      </pre>
-    </details>
+    <BoardDragAndDrop
+      :tasks="tasks"
+      :board="board"
+      @update="updateBoard"
+      :addTask="addTask"
+    />
   </div>
 </template>
 

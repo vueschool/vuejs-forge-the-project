cdc85312ebe3957f7d49da2f91b30e8273d8236a
Daniel Kelly <me@danielkelly.io>
2022-07-13 09:41:52 -0500

    exercise 1

--- components.d.ts
+++ components.d.ts
@@ -6,6 +6,8 @@ import '@vue/runtime-core'
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     AppButton: typeof import('./src/components/AppButton.vue')['default']
+    AppImage: typeof import('./src/components/AppImage.vue')['default']
+    BoardCard: typeof import('./src/components/BoardCard.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
